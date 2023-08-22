7c53cee12b940f0887efedf421a419a9646e75f7
tracer-ics2020 <tracer@njuics.org>
2023-08-22 16:32:08 +0800

    >  compile 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:32:09 up 1 day,  3:15,  1 user,  load average: 0.47, 0.29, 0.21

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -147,6 +147,7 @@ static inline def_rtl(j, vaddr_t target) {
 
 static inline def_rtl(jr, rtlreg_t *target) {
   s->jmp_pc = *target;
+  printf("jump to %#x\n", s->jmp_pc);
   s->is_jmp = true;
 }
 
