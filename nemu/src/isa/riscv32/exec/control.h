b04fbeaea88ef19c955b2eb89d4cd7b2c29b3619
tracer-ics2020 <tracer@njuics.org>
2023-08-22 16:35:42 +0800

    >  compile 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:35:42 up 1 day,  3:19,  1 user,  load average: 0.34, 0.36, 0.26

--- nemu/src/isa/riscv32/exec/control.h
+++ nemu/src/isa/riscv32/exec/control.h
@@ -3,6 +3,6 @@ static inline def_EHelper(jal)
     rtl_li(s, s0, s->seq_pc);
     rtl_addi(s, s1, s0, id_src1->simm);
     rtl_jr(s, s1);
-    rtl_addi(s, ddest, s0, 4);
+    // rtl_addi(s, ddest, s0, 4);
     print_asm_template2(jal);
 }
