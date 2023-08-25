1bae6f28973dc958bf83a29fd2a88f8be3f71fda
tracer-ics2020 <tracer@njuics.org>
2023-08-25 23:43:35 +0800

    >  run 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  23:43:35 up 1 day, 21:41,  1 user,  load average: 1.01, 0.76, 0.56

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -64,6 +64,7 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
+  putch('a');
   for (int i = 0; i < n; i++){
     if (((const unsigned char *)s1)[i] < ((const unsigned char *)s2)[i]){
       putch('<');
