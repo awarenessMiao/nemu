e136c5f37243c575b1bbcf5caaaa1cf9a6746280
tracer-ics2020 <tracer@njuics.org>
2023-08-25 22:28:40 +0800

    >  compile 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  22:28:40 up 1 day, 20:26,  1 user,  load average: 0.16, 0.31, 0.33

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,13 +4,13 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if (ref_r->pc != cpu.pc)
-    return false;
-  for (int i = 0; i < 32; i++) {
-    if (ref_r->gpr[i]._32 != cpu.gpr[i]._32)
-      return false;
-  }
-  return true;
+  // if (ref_r->pc != cpu.pc)
+  //   return false;
+  // for (int i = 0; i < 32; i++) {
+  //   if (ref_r->gpr[i]._32 != cpu.gpr[i]._32)
+  //     return false;
+  // }
+  return false;
 }
 
 void isa_difftest_attach() {
