76154aa0af098a5c6ede0d2993f8776c70217dfa
tracer-ics2020 <tracer@njuics.org>
2023-08-26 00:59:20 +0800

    >  run 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  00:59:20 up 1 day, 22:56,  1 user,  load average: 0.29, 0.24, 0.26

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -1,11 +1,17 @@
 #include <am.h>
 #include <nemu.h>
 
+uint32_t init_sec, init_usec;
+
 void __am_timer_init() {
+  init_usec = inl(RTC_ADDR);
+  init_sec = inl(RTC_ADDR + 4);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t uptime_usec = inl(RTC_ADDR);
+  uint32_t uptime_sec = inl(RTC_ADDR + 4);
+  uptime->us = (uptime_sec - init_sec)*1000000 + (uptime_usec - init_usec);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
