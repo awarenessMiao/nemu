0860739a826603a379a9107327c0caf38695e7cc
tracer-ics2020 <tracer@njuics.org>
2023-08-26 00:03:37 +0800

    >  run 21122119  Static hostname: fumiao-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 91d17e0527fe486194579f6a9ae2c272          Boot ID: 57216846127c4e4f869f2107f8159f21   Virtualization: vmware Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.0-1036-intel-iotg     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  00:03:37 up 1 day, 22:01,  1 user,  load average: 0.45, 0.46, 0.46

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,120 +4,73 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  assert(s != NULL);
-  // if (s == NULL)
-    // return 0;
-  size_t length_cnt = 0;
-  for (; s[length_cnt] != '\0'; length_cnt++)
-    ;
-  return length_cnt;
+  const char *p = s;
+  int len = 0;
+  while (*p != '\0'){
+    p++;
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  assert(dst != NULL && src != NULL);
-  int i = 0;
-  for (; src[i] != '\0'; i++)
-    dst[i] = src[i];
-  dst[i] = '\0';
-  return dst;
+  return memmove(dst, src, strlen(src)+1);
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  assert(dst != NULL && src != NULL);
-  int i = 0;
-  for (; src[i] != '\0' && i < n; i++)
-    dst[i] = src[i];
-  for (; i < n; i++)
-    dst[i] = '\0';
-  return dst;
+  int len = strlen(src) + 1;
+  return memmove(dst, src, n < len ? n : len);
 }
 
 char* strcat(char* dst, const char* src) {
-  int i = 0;
-  size_t len = strlen(dst);
-  for (; src[i] != '\0'; i++)
-    dst[len + i] = src[i];
-  dst[len + i] = '\0';
+  strcpy(dst + strlen(dst), src);
   return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  if (s1 == NULL)
-    return -1;
-  if (s2 == NULL)
-    return 1;
-  for (int i = 0;; i++)
-  {
-    if (*(s1 + i) == '\0' && *(s2 + i)=='\0')
-      return 0;
-    else if (*(s1 + i) == '\0' || *(s1 + i) < *(s2 + i))
-      return -1;
-    else if (*(s2 + i) == '\0' || *(s1 + i) > *(s2 + i))
-      return 1;
-  }
+  int len1 = strlen(s1), len2 = strlen(s2);
+  // int min_len = strlen(s1) > strlen(s2) ? strlen(s2) : strlen(s1);
+  return strncmp(s1, s2, len1 < len2 ? len1 : len2);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  if (s1 == NULL)
-    return -1;
-  if (s2 == NULL)
-    return 1;
-  for (int i = 0; i < n; i++)
-  {
-    if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
-      return 0;
-    else if (*(s1 + i) == '\0' || *(s1 + i) < *(s2 + i))
-      return -1;
-    else if (*(s2 + i) == '\0' || *(s1 + i) > *(s2 + i))
-      return 1;
-  }
-  return 0;
+  return memcmp(s1, s2, n);
 }
 
 void* memset(void* v,int c,size_t n) {
-  assert(v != NULL && n > 0);
-  for (int i = 0; i < n; i++)
-  {
-    char *p_v = (char *)v + i;
-    *p_v = c;
+  for (int i = 0; i < n;i++){
+    ((unsigned char *)v)[i] = c;
   }
   return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
-  assert(dst != NULL && src != NULL && n > 0);
-  char p_tmp[100];
-  // char *p_tmp;
-  // p_tmp = malloc(n);
-  memcpy(p_tmp, src, n);
-  memcpy(dst, p_tmp, n);
-  // free(p_tmp);
+  if(src > dst){
+    for (int i = 0; i < n; i++)
+      ((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+  }
+  if(src < dst){
+    for (int i = n-1; i >=0 ; i--)
+      ((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+  }
   return dst;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  // assert(out != NULL && in != NULL && n > 0);
-  for (int i = 0; i < n; i++)
-  {
-    char *p_out = (char *)out + i;
-    char *p_in = (char *)in + i;
-    *p_out = *p_in;
+  for (int i = 0; i < n; i++){
+    ((unsigned char *)out)[i] = ((const unsigned char *)in)[i];
   }
-  return out;
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  assert(s1 != NULL && s2 != NULL && n > 0);
-  for (int i = 0; i < n; i++)
-  {
-    char *p_s1 = (char *)s1 + i;
-    char *p_s2 = (char *)s2 + i;
-    if (*p_s1 < *p_s2)
+  for (int i = 0; i < n; i++){
+    if (((const unsigned char *)s1)[i] < ((const unsigned char *)s2)[i])
       return -1;
-    else if (*p_s1 > *p_s2)
+    if (((const unsigned char *)s1)[i] > ((const unsigned char *)s2)[i])
       return 1;
   }
-    return 0;
+  return 0;
 }
 
 #endif
